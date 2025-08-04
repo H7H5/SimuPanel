6692e4061a004d0d4b5a74dbbd44e8561c0919c0
H7H5 <Serdyk87@ukr.net>
2025-08-04 13:32:33 +0300

    1

---
+++ SimuPanel/Assets/Scripts/RotateON_OFF.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateON_OFF : MonoBehaviour
+{
+    private bool isOn = false;
+
+    private void Start()
+    {
+        ApplyRotation(isOn);
+    }
+
+    private void OnMouseDown()
+    {
+        isOn = !isOn;
+        ApplyRotation(isOn);
+        Debug.Log(isOn ? "Увімкнено" : "Вимкнено");
+    }
+
+    private void ApplyRotation(bool state)
+    {
+        float angleX = state ? -166f : -88f;
+
+        // Встановлюємо поворот напряму через кватерніон
+        transform.localRotation = Quaternion.Euler(angleX, 0f, 0f);
+    }
+
+}
