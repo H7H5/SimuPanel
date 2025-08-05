e8013a07dd8ed610ce3b9f39f1026ad6b7bb4e34
H7H5 <Serdyk87@ukr.net>
2025-08-05 14:14:45 +0300

    1

---
+++ SimuPanel/Assets/Scripts/CameraController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float sensitivity = 2.0f;
+
+    private float rotationX = 0.0f;
+    private float rotationY = 0.0f;
+    private bool isInteracting = false;
+    void Start()
+    {
+        //Cursor.lockState = CursorLockMode.None;
+        //Cursor.visible = true;
+        LockCursor(true);
+    }
+
+    void Update()
+    {
+        // Перемикання режиму пробілом
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isInteracting = !isInteracting;
+            LockCursor(!isInteracting);
+        }
+
+        if (!isInteracting)
+        {
+            float mouseX = Input.GetAxis("Mouse X") * sensitivity;
+            float mouseY = Input.GetAxis("Mouse Y") * sensitivity;
+
+            rotationX -= mouseY;
+            rotationX = Mathf.Clamp(rotationX, -90f, 90f);
+
+            rotationY += mouseX;
+
+            transform.localRotation = Quaternion.Euler(rotationX, rotationY, 0f);
+        }
+    }
+
+
+    void LockCursor(bool lockCursor)
+    {
+        Cursor.lockState = lockCursor ? CursorLockMode.Locked : CursorLockMode.None;
+        Cursor.visible = !lockCursor;
+    }
+}
---
+++ SimuPanel/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5.0f;
+    public Transform cameraTransform; // посилання на об'єкт камери
+    private CharacterController controller;
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 forward = cameraTransform.forward;
+        Vector3 right = cameraTransform.right;
+
+        // Ігноруємо вертикальну вісь (Y), щоб не було руху вгору/вниз
+        forward.y = 0f;
+        right.y = 0f;
+
+        forward.Normalize();
+        right.Normalize();
+
+        // Формуємо напрямок руху
+        Vector3 moveDir = forward * verticalInput + right * horizontalInput;
+
+        // Додати вертикальний рух (вгору/вниз)
+        if (Input.GetKey(KeyCode.E))
+        {
+            moveDir += Vector3.up;
+        }
+        else if (Input.GetKey(KeyCode.Q))
+        {
+            moveDir += Vector3.down;
+        }
+
+        controller.Move(moveDir * moveSpeed * Time.deltaTime);
+    }
+}
